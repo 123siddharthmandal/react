e9171c4b265378728510920651ffe83b153fc1b8
Siddharth Mandal <siddharthmandal525@gmail.com>
2024-05-13 11:36:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Dashboard from './components/dashboard';
+import Header from './components/header';
+import Node from './components/Node';
+import Nesting from './components/nesting';
+
+function App() {
+  return (
+    <>
+      <Header/>
+     <Dashboard name="ABC"/>
+     <Nesting/>
+     <Node/>
+    </>
+  );
+}
+
+export default App
